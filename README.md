71e8515d07e0fb695665874f43c32e31e1d625fa
Ribas <ygomes@ciandt.com>
2021-07-13 09:54:05 -0400

    Inicio do Alurakut

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
+## Environments
+
+  npm run dev
+    Starts the development server.
+
+  npm run build
+    Builds the app for production.
+
+  npm start
+    Runs the built app in production mode.
+
+  We suggest that you begin by typing:
+
+  yacd alurakut-react
+  npm run dev
+
+
 # Example app with styled-components
 
 This example features how you use a different styling solution than [styled-jsx](https://github.com/vercel/styled-jsx) that also supports universal styles. That means we can serve the required styles for the first render within the HTML and then load the rest in the client. In this case we are using [styled-components](https://github.com/styled-components/styled-components).
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,104 @@
 import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
 
+function ProfileSideBar(properties) {
+  return (
+    <Box>
+      <img style={{
+        borderRadius: '8px',
+        width: '121px',
+        height: '121px'
+      }} src={`https://github.com/${properties.githubUser}.png`} />
+      <ProfileContent>
+        <h6>  </h6>
+        <p>A simple description with linebreaks</p>
+      </ProfileContent>
+      <ProfileContent>
+        <ProfileUlDesign>
+          <li><Links href="#">Perfil</Links></li>
+          <li><Links href="#">RecLinksdos</Links></li>
+          <li><Links href="#">Fotos</Links></li>
+          <li><Links href="#">Vídeos</Links></li>
+          <li><Links href="#">Depoimentos</Links></li>
+        </ProfileUlDesign>
+      </ProfileContent>
+    </Box>
+  );
+}
+
+const ProfileContent = styled.div`
+  border-top: 1px solid #ECF2FA;
+  padding:16px;
+`
+
+const ProfileUlDesign = styled.ul`
+  list-style-type: none;
+`
+
+const Links = styled.a`
+  font-family: Rubik;
+  text-decoration: none;
+  color: #2E7BB4;
+  font-weight: 400;
+  font-size: 12px;
+`
+
 export default function Home() {
-  return <Title>My page</Title>
+
+  const githubUser = 'YghoRibas';
+  const persons = [
+    'juunegreiros',
+    'omariosouto',
+    'peas',
+    'rafaballerini',
+    'marcobrunodev',
+    'felipefialho'
+  ]
+
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
+          <ProfileSideBar githubUser={githubUser} />
+        </div>
+        <div className="feedArea" style={{ gridArea: "feedArea" }}>
+          <Box> 
+            <h1 className="title">
+              Bem vindo(a)
+            </h1>
+            <OrkutNostalgicIconSet/>
+            </Box>
+        </div>
+        <div className="comunityArea" style={{ gridArea: "comunityArea" }}>
+          <Box >
+            <ProfileRelationsBoxWrapper>
+              <h2 className="smallTitle"> Pessoas Favoritas ({persons.length})</h2>
+              <ul>
+                {persons.map((itemAtual) => {
+                  return (
+                    <li>
+                      <Links href={`/users/${itemAtual}`} key={itemAtual}>
+                        <img src={`https://github.com/${itemAtual}.png`} />
+                        <span>{itemAtual}</span>
+                      </Links>
+                    </li>
+                  )
+                })}
+              </ul> 
+              </ProfileRelationsBoxWrapper>
+            </Box>
+          <Box > Comunidades </Box>
+        </div>
+      </MainGrid>
+    </>
+  )
 }
